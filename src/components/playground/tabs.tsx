ee1c72af2c03a81a1c4cffdce2fabf1c7d738490
Nauval <nauvalazhar2@gmail.com>
2026-05-16 12:53:27 +0700

    fix(playground): use bg-accent for tabs indicator

--- src/components/playground/tabs.tsx
+++ src/components/playground/tabs.tsx
@@ -33,7 +33,7 @@ export function TabsList({
       {children}
       <BaseTabs.Indicator
         className={cn(
-          "absolute top-1/2 left-0 z-[-1] h-7.5 w-(--active-tab-width) translate-x-(--active-tab-left) -translate-y-1/2 rounded-xl bg-site-accent transition-all duration-200 ease-in-out",
+          "absolute top-1/2 left-0 z-[-1] h-7.5 w-(--active-tab-width) translate-x-(--active-tab-left) -translate-y-1/2 rounded-xl bg-accent transition-all duration-200 ease-in-out",
         )}
       />
     </BaseTabs.List>
